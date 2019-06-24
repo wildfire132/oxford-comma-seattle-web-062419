22596ea1be58470f0f0b2beff65fe22476bfae1b
Katana Tran <tkatyk21@gmail.com>
2019-06-24 18:00:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   array.insert(-2, "and")
   for item in (0..array.length-2)
     res += array[item]
-    if item < (array.length-2)
+    if array.length > 2 && item < (array.length-2)
       res += ","
     end
     res += " "
