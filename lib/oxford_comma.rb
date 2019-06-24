c756658185de506a2ace1f3c28b965aa0ee389e8
Katana Tran <tkatyk21@gmail.com>
2019-06-24 17:58:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length >= 3
+    str = array.join(", ")
+    array = str.split(" ")
+    array.insert(-2,"and")
+    array.join(" ")
+  else 
+    array.join(" and ")
+  end 
 end
